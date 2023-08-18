cdd58dc0450569c14dfcee0f3107da24e45006c5
JasSingh2003 <133809114+JasSingh2003@users.noreply.github.com>
2023-08-18 18:49:35 -0400

    Update GroupOfCards.java

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -5,21 +5,21 @@ import java.util.Collections;
 import java.util.List;
 
 public class GroupOfCards {
-    private final List<Card> cards;
+    private final List<Card> cards;  //declaring a list of cards
 
     public GroupOfCards() {
-        cards = new ArrayList<>();
+        cards = new ArrayList<>();    //constructor to initialize the list of cards
     }
 
-    public void addCard(Card card) {
+    public void addCard(Card card) {   //method to add card to group of cards
         cards.add(card);
     }
 
-    public void shuffle() {
+    public void shuffle() {   //method to shuffle cards
         Collections.shuffle(cards);
     }
 
-    public Card drawCard() {
+    public Card drawCard() {    //method to draw a card from the group returning null if group is empty.
         if (cards.isEmpty()) {
             return null;
         }
