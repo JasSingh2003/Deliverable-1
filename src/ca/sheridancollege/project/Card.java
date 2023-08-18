f9449dc3c322ef25605e953d374a8b2322fd6418
JasSingh2003 <133809114+JasSingh2003@users.noreply.github.com>
2023-08-18 18:45:56 -0400

    Update Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -1,25 +1,25 @@
 package ca.sheridancollege.project;
 
-public abstract class Card {
+public abstract class Card {    //variable for cards
    protected final String suit;
    protected final String rank;
 
-    public Card(String suit, String rank) {
+    public Card(String suit, String rank) {   //constructor to initialize cards
         this.suit = suit;
         this.rank = rank;
     }
 
-    public String getSuit() {
+    public String getSuit() {     //accessors for suit of card
         return suit;
     }
 
-    public String getRank() {
+    public String getRank() {   //accessor for rank of card
         return rank;
     }
 
     
-    public abstract int getValue();
+    public abstract int getValue();    //abstract accessor of card
     
     @Override
-    public abstract String toString();
+    public abstract String toString();    //abstract method to print the value of card
 }
